c0e54e477c95a99a3054db3c530b3d97f616d2a1
AmitSakhare <66629728+AmitSakhare@users.noreply.github.com>
2025-07-02 13:31:22 +0530

    commit

--- Sales_2022.sql
+++ Sales_2022.sql
@@ -15,6 +15,12 @@ where Billdate between '2022-04-01' And '2022-04-30'  group by Distributor,Billd
 select DISTINCT Distributor as PartyName,ConsigneeName, avg(round((GAmount/TotWeight),2)) as rate 
 from Sales1 where Distributor is not null group by Distributor,ConsigneeName;
 
+-- Monthwise Sales
+select Distributor,
+datename(mm,Billdate)as month_of_sale,round(sum(NetAmount),2) as Sale_amount  
+from Sales1 group by Distributor,Billdate;
 
-
-
+-- Monthly Sales With Rate
+select Distributor,DATENAME(mm,Billdate) as month_of_sale,
+avg(round(avg(GAmount/TotWeight),2)) over (partition by Distributor order by DATENAME(mm,Billdate)) as Sales_2022
+from Sales1 where Distributor is not null group by Distributor,Billdate;
