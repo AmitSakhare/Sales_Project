97ef0735fd0e5428a349b8301666fb471b7263db
AmitSakhare <66629728+AmitSakhare@users.noreply.github.com>
2025-07-02 13:07:14 +0530

    Commit

--- Sales_2022.sql
+++ Sales_2022.sql
@@ -1,1 +1,20 @@
+--WAQ to  Featch all data 
 select * from Sales1;
+
+--WAQ to Null PartyNull Value to Change Name
+Select *,ISNULL(Distributor,'ExportParty') as PartyName from Sales1;
+
+-- WAQ to Sales of Local and Export
+select round(sum(NetAmount),2),Description as Area from Sales1 group by Description;
+
+-- WAQ to MonthWise sales 
+select Distributor as PartyName, Billdate, sum(NetAmount) from Sales1 
+where Billdate between '2022-04-01' And '2022-04-30'  group by Distributor,Billdate ;
+
+-- WAQ To Find Avg Rates Distributor and ConsigneeName wise
+select DISTINCT Distributor as PartyName,ConsigneeName, avg(round((GAmount/TotWeight),2)) as rate 
+from Sales1 where Distributor is not null group by Distributor,ConsigneeName;
+
+
+
+
